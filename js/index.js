8eeb56aa0e5231195c820003f3289d66713c562d
Codrut06 <130077895+Codrut06@users.noreply.github.com>
2024-03-28 17:21:36 +0200

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,13 @@
+  (function ($) {
+  
+  "use strict";
+
+    // AOS ANIMATIONS
+    AOS.init();
+
+    // NAVBAR
+    $('.navbar-nav .nav-link').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+    
+  })(window.jQuery);
